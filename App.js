3f72832c64e7845b52a07b978f64ee69fdf9e169
Nicole Wong <nhswong@gmail.com>
2017-03-15 02:41:39 -0700

    add reducers and action types in noteListRedux.js

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import { connect } from 'react-redux'
+
+import { actionCreators } from './src/notesListRedux'
+import List from './src/components/List'
+import Input from './src/components/Input'
+import Header from './src/components/Header'
+
+
+const mapStateToProps = (state) => ({
+  notes: state.notes,
+})
 
 class App extends Component {
+  onAddNotes = (text) => {
+    const { dispatch } = this.props
+
+    // `dispatch(action)` is a Store Method. it dispatches an action. this is the only way to trigger a state change.
+    dispatch(actionCreators.add(text))
+  }
+
+  onRemoveNotes = (index) => {
+    const { dispatch } = this.props
+
+    dispatch(actionCreators.remove(index))
+  }
+
   render() {
+    const {notes} = this.props
+
     return (
       <View>
-
+        {/* <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo} */}
+        
+        <List
+          list={notes}
+          onPressItem={this.onRemoveNotes}
+        />
       </View>
-    );
+    )
   }
 }
 
 export default App;
+
+// `connect(mapStateToProps)` will help the new component subscribe to Redux store updates -- part of react-redux
